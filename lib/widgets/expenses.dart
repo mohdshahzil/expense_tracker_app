2b962bdbeb2f9d63d81499bf8e5381263e31e528
mohdshahzil <theshahzil@gmail.com>
2024-03-10 03:21:23 +0530

    dismissible list

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -38,6 +38,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +62,7 @@ class _ExpensesState extends State<Expenses> {
           Expanded(
             child: ExpensesList(
               expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
             ),
           ),
         ],
