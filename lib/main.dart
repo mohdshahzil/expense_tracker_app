27836d50db506a29448e77d0ab36e8f41d799afe
mohdshahzil <theshahzil@gmail.com>
2024-03-10 04:05:33 +0530

    themes

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 141, 107, 219),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+
+        // appBarTheme: const AppBarTheme().copyWith(
+        //     backgroundColor: kColorScheme.onPrimaryContainer,
+        //     foregroundColor: kColorScheme.primaryContainer),
+      ),
       home: const Expenses(),
     ),
   );
